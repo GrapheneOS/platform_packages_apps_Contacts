5dcb1f3dcc5794adc3a63855116f4d2f9438194f
Pratyush <codelab@pratyush.dev>
2022-11-24 23:07:21 +0530

    migrate dialog to androidx dialog

--- src/com/android/contacts/editor/LabeledEditorView.java
+++ src/com/android/contacts/editor/LabeledEditorView.java
@@ -16,7 +16,7 @@
 
 package com.android.contacts.editor;
 
-import android.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
