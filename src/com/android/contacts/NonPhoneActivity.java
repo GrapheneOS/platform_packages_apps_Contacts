05393e218bbba9f11ebb7646ec057efe96fe12ac
Pratyush <codelab@pratyush.dev>
2022-11-24 23:07:21 +0530

    migrate dialog to androidx dialog

--- src/com/android/contacts/NonPhoneActivity.java
+++ src/com/android/contacts/NonPhoneActivity.java
@@ -17,7 +17,7 @@
 package com.android.contacts;
 
 import android.app.Activity;
-import android.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
