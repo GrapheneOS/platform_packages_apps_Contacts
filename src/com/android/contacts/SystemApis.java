f21e55ff3e97e51313d6766fc45003f9f75ac12e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-19 16:19:51 +0200

    inline SET_DEFAULT_ACCOUNT_FOR_CONTACTS permission system API constant

---
+++ src/com/android/contacts/SystemApis.java
@@ -0,0 +1,7 @@
+package com.android.contacts;
+
+public class SystemApis {
+    // android.Manifest.permission.SET_DEFAULT_ACCOUNT_FOR_CONTACTS
+    public static final String SET_DEFAULT_ACCOUNT_FOR_CONTACTS =
+            "android.permission.SET_DEFAULT_ACCOUNT_FOR_CONTACTS";
+}
