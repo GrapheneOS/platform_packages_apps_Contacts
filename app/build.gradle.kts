20a75d0b542b46db172b89d2e39416698c198875
Artem Smirnov <artem@smirnov.page>
2026-04-10 21:23:14 +0300

    Improve dependencies ordering

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,22 +73,27 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.palette)
+    implementation(libs.androidx.swiperefreshlayout)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.palette)
-    implementation(libs.androidx.swiperefreshlayout)
-    implementation(libs.guava)
+
     implementation(libs.hilt.android)
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.material)
     ksp(libs.hilt.compiler)
+
+    implementation(libs.guava)
+
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.material)
 
     implementation(project(":lib:platform_external_libphonenumber"))
     implementation(project(":lib:platform_frameworks_ex:common"))
