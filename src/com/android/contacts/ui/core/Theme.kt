51c177272e4ef964caa6aaed22b0e6bd37fa3e21
Artem Smirnov <artem@smirnov.page>
2026-04-10 19:51:14 +0300

    Add Compose dependencies

---
+++ src/com/android/contacts/ui/core/Theme.kt
@@ -0,0 +1,36 @@
+package com.android.contacts.ui.core
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+private val AppShapes = Shapes(
+    extraSmall = RoundedCornerShape(size = 12.dp),
+    small = RoundedCornerShape(size = 16.dp),
+    medium = RoundedCornerShape(size = 20.dp),
+    large = RoundedCornerShape(size = 28.dp),
+    extraLarge = RoundedCornerShape(size = 36.dp),
+)
+
+@Composable
+fun AppTheme(
+    content: @Composable () -> Unit,
+) {
+    val context = LocalContext.current
+    val colorScheme = when {
+        isSystemInDarkTheme() -> dynamicDarkColorScheme(context = context)
+        else -> dynamicLightColorScheme(context = context)
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = AppShapes,
+        content = content,
+    )
+}
