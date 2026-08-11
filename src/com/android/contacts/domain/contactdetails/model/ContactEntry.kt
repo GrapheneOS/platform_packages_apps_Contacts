15f6acffdcec074f000609713872cf8cd788ec0a
Matvei Plokhov <mpplokho@gmail.com>
2026-08-11 09:57:28 +0200

    Add contact entry actions

--- src/com/android/contacts/domain/contactdetails/model/ContactEntry.kt
+++ src/com/android/contacts/domain/contactdetails/model/ContactEntry.kt
@@ -3,10 +3,11 @@ package com.android.contacts.domain.contactdetails.model
 internal data class ContactEntry(
     val id: Long,
     val mimeType: String?,
+    val isSuperPrimary: Boolean,
     val header: ContactEntryText?,
     val subHeader: String?,
     val text: String?,
-    val isSuperPrimary: Boolean,
     val copyText: String?,
     val copyLabel: ContactEntryText?,
+    val actions: ContactEntryActions,
 )
