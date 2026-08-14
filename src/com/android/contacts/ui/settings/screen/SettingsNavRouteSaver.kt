f707970e8685df7c2123f183023f4eb854a497a5
Matvei Plokhov <mpplokho@gmail.com>
2026-08-14 23:58:08 +0200

    Rework settings navigation

--- src/com/android/contacts/ui/settings/screen/SettingsNavRouteSaver.kt
+++ src/com/android/contacts/ui/settings/screen/SettingsNavRouteSaver.kt
@@ -3,19 +3,11 @@ package com.android.contacts.ui.settings.screen
 import androidx.compose.runtime.saveable.Saver
 import com.android.contacts.ui.settings.screen.model.SettingsNavRoute
 
-private const val MAIN_ROUTE = "main"
-private const val ABOUT_ROUTE = "about"
-
 internal val SettingsNavRouteSaver: Saver<SettingsNavRoute, String> = Saver(
-    save = { route ->
-        when (route) {
-            SettingsNavRoute.Main -> MAIN_ROUTE
-            SettingsNavRoute.About -> ABOUT_ROUTE
-        }
-    },
+    save = { route -> route.key },
     restore = { savedRoute ->
         when (savedRoute) {
-            ABOUT_ROUTE -> SettingsNavRoute.About
+            SettingsNavRoute.About.key -> SettingsNavRoute.About
             else -> SettingsNavRoute.Main
         }
     },
