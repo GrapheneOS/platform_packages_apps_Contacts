3dcb74b433c051a4253977b3afe76fa752fb4819
Sérgio Santos <me@sergiosantos.info>
2026-09-08 10:45:12 +0100

    Fix bottom sheet behaviour. Improve phone formatting. Show single SIM card string.

--- src/com/android/contacts/ui/interactions/importing/screen/model/SimCardOption.kt
+++ src/com/android/contacts/ui/interactions/importing/screen/model/SimCardOption.kt
@@ -1,11 +1,12 @@
 package com.android.contacts.ui.interactions.importing.screen.model
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.AnnotatedString
 
 @Immutable
 internal data class SimCardOption(
     val subscriptionId: Int,
     val name: String? = null,
     val contactsCount: Int? = null,
-    val phone: String? = null,
+    val phone: AnnotatedString? = null,
 )
