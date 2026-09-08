2838c3fbaf6e4b05ca3e63fee9f34facc5be7f76
Sérgio Santos <me@sergiosantos.info>
2026-09-08 10:45:12 +0100

    Fix bottom sheet behaviour. Improve phone formatting. Show single SIM card string.

--- src/com/android/contacts/ui/interactions/importing/screen/mapper/SimCardOptionMapper.kt
+++ src/com/android/contacts/ui/interactions/importing/screen/mapper/SimCardOptionMapper.kt
@@ -1,8 +1,9 @@
 package com.android.contacts.ui.interactions.importing.screen.mapper
 
-import androidx.core.text.BidiFormatter
-import androidx.core.text.TextDirectionHeuristicsCompat
-import com.android.contacts.compat.PhoneNumberUtilsCompat
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.VerbatimTtsAnnotation
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withAnnotation
 import com.android.contacts.model.SimCard
 import com.android.contacts.ui.interactions.importing.screen.model.SimCardOption
 import javax.inject.Inject
@@ -11,26 +12,22 @@ internal fun interface SimCardOptionMapper {
     fun map(simCard: SimCard): SimCardOption
 }
 
-internal class SimCardOptionMapperImpl @Inject constructor(
-    private val bidiFormatter: BidiFormatter,
-) : SimCardOptionMapper {
+internal class SimCardOptionMapperImpl @Inject constructor() : SimCardOptionMapper {
     override fun map(simCard: SimCard): SimCardOption {
         return SimCardOption(
             subscriptionId = simCard.subscriptionId,
             name = simCard.displayName?.toString(),
             contactsCount = simCard.contacts?.size,
-            phone = getFormattedPhone(simCard),
+            phone = annotatedPhone(simCard),
         )
     }
 
-    private fun getFormattedPhone(simCard: SimCard): String? {
-        return (simCard.getFormattedPhone() ?: simCard.phone)
-            ?.ifEmpty { null }
-            ?.let { phone ->
-                bidiFormatter.unicodeWrap(
-                    PhoneNumberUtilsCompat.createTtsSpannable(phone),
-                    TextDirectionHeuristicsCompat.LTR,
-                )?.toString()
+    private fun annotatedPhone(simCard: SimCard): AnnotatedString {
+        val phone = simCard.getFormattedPhone() ?: simCard.phone
+        return buildAnnotatedString {
+            withAnnotation(VerbatimTtsAnnotation(phone.toString())) {
+                append(phone)
             }
+        }
     }
 }
