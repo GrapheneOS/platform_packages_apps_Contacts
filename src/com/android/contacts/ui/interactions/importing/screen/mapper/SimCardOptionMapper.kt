17fda2abc3aaf0f8ffd02210ffc476c592ddc43f
Sérgio Santos <me@sergiosantos.info>
2026-09-09 15:28:08 +0100

    Protect SimCardOptionMapper from NPE. Simplify simCardOptionDescription and ensure annotations are kept.

--- src/com/android/contacts/ui/interactions/importing/screen/mapper/SimCardOptionMapper.kt
+++ src/com/android/contacts/ui/interactions/importing/screen/mapper/SimCardOptionMapper.kt
@@ -1,9 +1,12 @@
 package com.android.contacts.ui.interactions.importing.screen.mapper
 
+import android.telephony.PhoneNumberUtils
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.VerbatimTtsAnnotation
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withAnnotation
+import androidx.core.text.BidiFormatter
+import androidx.core.text.TextDirectionHeuristicsCompat
 import com.android.contacts.model.SimCard
 import com.android.contacts.ui.interactions.importing.screen.model.SimCardOption
 import javax.inject.Inject
@@ -12,7 +15,9 @@ internal fun interface SimCardOptionMapper {
     fun map(simCard: SimCard): SimCardOption
 }
 
-internal class SimCardOptionMapperImpl @Inject constructor() : SimCardOptionMapper {
+internal class SimCardOptionMapperImpl @Inject constructor(
+    private val bidiFormatter: BidiFormatter,
+) : SimCardOptionMapper {
     override fun map(simCard: SimCard): SimCardOption {
         return SimCardOption(
             subscriptionId = simCard.subscriptionId,
@@ -22,11 +27,20 @@ internal class SimCardOptionMapperImpl @Inject constructor() : SimCardOptionMapp
         )
     }
 
-    private fun annotatedPhone(simCard: SimCard): AnnotatedString {
-        val phone = simCard.getFormattedPhone() ?: simCard.phone
+    private fun annotatedPhone(simCard: SimCard): AnnotatedString? {
+        val phone = (simCard.getFormattedPhone() ?: simCard.phone)
+            ?.takeIf { it.isNotBlank() }
+            ?: return null
+        val wrappedPhone = bidiFormatter.unicodeWrap(
+            PhoneNumberUtils.createTtsSpannable(phone),
+            TextDirectionHeuristicsCompat.LTR,
+        )
+            .takeIf { it.isNotBlank() }
+            ?: phone
+
         return buildAnnotatedString {
             withAnnotation(VerbatimTtsAnnotation(phone.toString())) {
-                append(phone)
+                append(wrappedPhone)
             }
         }
     }
